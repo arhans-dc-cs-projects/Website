2a836bfd8bad68e54e6227239584df96da16e721
Arhan <arhanh1234@gmail.com>
2022-11-08 13:21:27 +0400

    Create profitornot.js

---
+++ js/profitornot.js
@@ -0,0 +1,10 @@
+script>
+    var x = parseInt(prompt("What was the cost of the product?"))
+    var y = parseInt(prompt("What was the cost you sold it for?"))
+    if(x>y){
+    	document.write("You did not make profit.")
+    }
+    else{
+    	document.write("You made profit.")
+    }
+</script>
